3523cc25ec5f9eb517ff3e7061dcaead7549b894
majesova <majesova@gmail.com>
2018-07-12 17:56:19 -0500

    Modelos iniciales

---
+++ Furnies.WebUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Furnies.WebUI.Startup))]
+namespace Furnies.WebUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
---
+++ Pruebas/Program.cs
@@ -0,0 +1,20 @@
+﻿using Furnies.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pruebas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FurniesContext ctx = new FurniesContext();
+            var mediciones = ctx.ConfiguracionesMedicion;
+            Console.WriteLine(mediciones.Count());
+            Console.ReadKey();
+        }
+    }
+}
